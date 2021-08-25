12e4062169549703837e6208756bcff6d1427f6c
Julian Garcia Ortiz <jugarcia01@hotmail.com>
2021-08-24 20:32:10 -0500

    Se incluyen mas archivos de codigo.

--- basicGoExamples/ch2/tuplasDeVariab/maxComunDivisor.go
+++ basicGoExamples/ch2/tuplasDeVariab/maxComunDivisor.go
@@ -1,6 +1,8 @@
 // Programa que realiza el cálculo del Maximo Común Divisor entre dos nuneros enteros.
+// https://sites.google.com/site/preprecalculo/unidad-1/calcula-el-mcm-minimo-comun-multiplo-y-mcd-maximo-comun-divisor
 
-package tuplasDeVariab
+//package tuplasDeVariab
+package main
 
 import(
   "fmt"
@@ -8,5 +10,45 @@ import(
 )
 
 func main() {
-  
+  fmt.Print("PROGRAMA PARA CALCULAR EL MAXIMO COMUN DIVISOR ENTRE DOS NUMEROS\nEs decir dados dos  num. x y;  Result = MCD(x,y)\n")
+
+  isNum1 := false
+		for !isNum1 {
+      fmt.Println("\nIngrese el 1er  número entero: ")
+			var dato1 string
+			fmt.Scanf("%s\n", &dato1)
+			x, err := strconv.Atoi(dato1)
+			if err != nil {
+				fmt.Println("ERROR: Debe ingresar un número entero válido.")
+				isNum1 = false
+      } else {
+        isNum1 = true
+        isNum2 := false
+          for !isNum2 {
+            fmt.Println("\nIngrese el 2do  número entero: ")
+			      var dato2 string
+			      fmt.Scanf("%s\n", &dato2)
+			      y, err := strconv.Atoi(dato2)
+			      if err != nil {
+				      fmt.Println("ERROR: Debe ingresar un número entero válido.")
+				      isNum2 = false
+            } else if y == 0 {
+				      fmt.Println("ERROR: Debe ingresar un número distinto de cero 0.")
+            } else {
+              result := mcd(x,y)
+
+				      fmt.Printf("\nEl Max.ComunDivisor de %d y %d es %d.\n", x, y, result)
+				      isNum2 = true
+          }
+			}
+    }
+  }
+}
+
+// Realiza el cálculo del máximo común divisor "MCD" entre dos números enteros dados x, y.
+func mcd(x, y int) int {
+  for y != 0 {
+    x, y = y, x%y
+  }
+  return x
 }
